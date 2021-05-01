d0b8717a7c0e728a45356944b17b36b0b4170f40
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2021-05-02 00:20:16 +0200

    Improve punchline layout

--- app/src/main/java/com/ipereziriarte/jokebook/ui/punchline/Punchline.kt
+++ app/src/main/java/com/ipereziriarte/jokebook/ui/punchline/Punchline.kt
@@ -1,31 +1,47 @@
 package com.ipereziriarte.jokebook.ui.punchline
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ipereziriarte.jokebook.ui.home.NavigationActions
+import com.ipereziriarte.jokebook.ui.theme.Teal200
 
 @Composable
 internal fun Punchline(actions: NavigationActions, punchline: String = "") {
-    Surface(color = MaterialTheme.colors.surface) {
-        Column {
-            Text(text = punchline)
-            Spacer(modifier = Modifier.size(32.dp))
-            Button(
-                onClick = { actions.navigateBack() }, modifier = Modifier.padding(8.dp),
-                shape = RoundedCornerShape(20.dp)
-            ) {
-                Text(text = "Back")
+
+    PunchlineCard(actions, punchline)
+}
+
+@Composable
+internal fun PunchlineCard(actions: NavigationActions, punchline: String = "") {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(onClick = { actions.navigateBack() }),
+            shape = RoundedCornerShape(topStart = 8.dp, bottomEnd = 8.dp),
+            backgroundColor = Teal200,
+        ) {
+            Column(modifier = Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = punchline, style = MaterialTheme.typography.h2)
             }
+
         }
     }
+
 }
