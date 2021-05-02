6549bb828ef946d3688e6b9208c0f311feab8894
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2021-05-02 18:59:21 +0200

    Add some animation to the cover

--- app/src/main/java/com/ipereziriarte/jokebook/ui/cover/Cover.kt
+++ app/src/main/java/com/ipereziriarte/jokebook/ui/cover/Cover.kt
@@ -1,11 +1,21 @@
 package com.ipereziriarte.jokebook.ui.cover
 
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -14,6 +24,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -24,11 +35,24 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.ipereziriarte.jokebook.R
 import com.ipereziriarte.jokebook.ui.navigation.NavigationActions
+import com.ipereziriarte.jokebook.ui.theme.Teal200
 
 @Composable
 internal fun Cover(actions: NavigationActions) {
-    Surface(color = MaterialTheme.colors.surface) {
-        Column(modifier = Modifier.padding(16.dp)) {
+
+    // Let's animate the image a little bit
+    val infiniteTransition = rememberInfiniteTransition()
+    val infinitelyAnimatedFloat = infiniteTransition.animateFloat(
+        initialValue = 0.2f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1500, easing = LinearOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Surface(color = Teal200) {
+        Column(modifier = Modifier.padding(16.dp).fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly) {
             Text(
                 text = stringResource(id = R.string.cover_title),
                 style = MaterialTheme.typography.h2,
@@ -40,8 +64,10 @@ internal fun Cover(actions: NavigationActions) {
                 contentDescription = "Jokes",
                 modifier = Modifier
                     .height(180.dp)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Crop
+                    .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Fit,
+                alpha = infinitelyAnimatedFloat.value
             )
             Spacer(modifier = Modifier.size(16.dp))
             Button(
@@ -49,8 +75,9 @@ internal fun Cover(actions: NavigationActions) {
                 modifier = Modifier
                     .padding(8.dp)
                     .align(Alignment.CenterHorizontally)
+                    .height(48.dp)
                     .testTag("CoverMainBtn"),
-                shape = RoundedCornerShape(20.dp)
+                shape = RoundedCornerShape(20.dp),
             ) {
                 Text(text = stringResource(id = R.string.cover_button))
             }
